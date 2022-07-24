a80cbab96548d2f88602257add20083f4b9531a6
Carrington <mcarringtone@gmail.com>
2022-07-24 16:38:56 -0400

    innerHTML

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 let output = document.querySelector("output");
+
+btn.addEventListener("click", () => {
+  let hours = document.querySelector("#hours").value;
+  document.querySelector("#output").innerHTML = hours;
+});
